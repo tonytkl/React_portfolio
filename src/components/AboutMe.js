0c2bfdf3ae035c8f35bed784f01c51270f8ebed1
Tony K <thitiklinpon@gmail.com>
2024-01-10 11:04:04 -0500

    Edit Home section to be rendered content from data.json

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,18 +1,30 @@
-import { Container, Row } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { Container, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import data from "../dataSource/data.json";
 
 function AboutMe() {
-    return <Container className='lightSection sections' id='aboutMe'>
-        <Row className='p-5'>
-            <h2 className='secondaryHeader'>About me</h2>
-            <p>My name is Tony Klinpon. I am a computer programming student at Humber college. I have some specialty in front-end developing using ReactJS and would love to explore in the back-end programming as well. I also have professional experience on data warehouse and ETL management.</p>
-            <div>
-                <a href="https://www.linkedin.com/in/tony-klinpon/" target="_blank"><FontAwesomeIcon icon={faLinkedin} className='icon' /></a>
-                <a href="https://www.github.com/tonytkl" target="_blank"><FontAwesomeIcon icon={faGithub} className='icon' /></a>
-            </div>
-        </Row>
+  return (
+    <Container className="lightSection sections" id="aboutMe">
+      <Row className="p-5">
+        <h2 className="secondaryHeader">About me</h2>
+        <p>
+          My name is Tony Klinpon. I am a computer programming student at Humber
+          college. I have some specialty in front-end developing using ReactJS
+          and would love to explore in the back-end programming as well. I also
+          have professional experience on data warehouse and ETL management.
+        </p>
+        <div>
+          <a href="https://www.linkedin.com/in/tony-klinpon/" target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} className="icon" />
+          </a>
+          <a href="https://www.github.com/tonytkl" target="_blank">
+            <FontAwesomeIcon icon={faGithub} className="icon" />
+          </a>
+        </div>
+      </Row>
     </Container>
+  );
 }
 
 export default AboutMe;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,40 @@
-import profileImg from '../images/profile.png';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import resume from '../document/Resume_SoftwareDeveloper.pdf';
+import { Container, Row, Col, Button } from "react-bootstrap";
+import data from "../dataSource/data.json";
 
 function Home() {
-    return <Container id='home' className='sections' >
-        <Row className='p-4 align-items-center'>
-            <Col md={6} lg={5} className='p-2'>
-                <a href="https://www.flaticon.com/authors/amethystdesign" title="AmethystDesign"><img id="mainImg" src={profileImg} /></a>
-            </Col>
-            <Col md={6} lg={7} className='p-4'>
-                <h1 id="mainHeader">Hi, I'm <span className='highlight'>Tony</span></h1>
-                <h2 className='secondaryHeader'>I'm a software developer</h2>
-                <div className='centerButton'>
-                    <Button variant="light" href={resume} download="Resume_Tony">Resume</Button>
-                    <Button variant="secondary" href={"#projects"}>Projects</Button>
-                </div>
-            </Col>
-        </Row>
+  var home = data.home;
+  return (
+    <Container id="home" className="sections">
+      <Row className="p-4 align-items-center">
+        <Col md={6} lg={5} className="p-2">
+          <a
+            href="https://www.flaticon.com/authors/amethystdesign"
+            title="AmethystDesign"
+          >
+            <img id="mainImg" src={require("../images/" + home.image)} />
+          </a>
+        </Col>
+        <Col md={6} lg={7} className="p-4">
+          <h1 id="mainHeader">
+            Hi, I'm <span className="highlight">{home.name}</span>
+          </h1>
+          <h2 className="secondaryHeader">I'm a {home.title}</h2>
+          <div className="centerButton">
+            <Button
+              variant="light"
+              href={require("../document/" + home.resume)}
+              download="Resume_Tony"
+            >
+              Resume
+            </Button>
+            <Button variant="secondary" href={"#projects"}>
+              Projects
+            </Button>
+          </div>
+        </Col>
+      </Row>
     </Container>
+  );
 }
 
 export default Home;
