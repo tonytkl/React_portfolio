a32aff26c41e4c426d148819b4fdee7d41c3a351
Tony K <thitiklinpon@gmail.com>
2024-01-10 13:37:39 -0500

    Edit the AboutMe section to render from data.json

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,26 +1,54 @@
 import { Container, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faLinkedin,
+  faGithub,
+  faInstagram,
+  faTwitch,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import data from "../dataSource/data.json";
 
 function AboutMe() {
+  var aboutMe = data.aboutMe;
   return (
     <Container className="lightSection sections" id="aboutMe">
       <Row className="p-5">
         <h2 className="secondaryHeader">About me</h2>
-        <p>
-          My name is Tony Klinpon. I am a computer programming student at Humber
-          college. I have some specialty in front-end developing using ReactJS
-          and would love to explore in the back-end programming as well. I also
-          have professional experience on data warehouse and ETL management.
-        </p>
+        <p>{aboutMe.text}</p>
         <div>
-          <a href="https://www.linkedin.com/in/tony-klinpon/" target="_blank">
-            <FontAwesomeIcon icon={faLinkedin} className="icon" />
-          </a>
-          <a href="https://www.github.com/tonytkl" target="_blank">
-            <FontAwesomeIcon icon={faGithub} className="icon" />
-          </a>
+          {aboutMe.link.map((link) => {
+            if (link.name == "linkedin")
+              return (
+                <a href={link.url} target="_blank">
+                  <FontAwesomeIcon icon={faLinkedin} className="icon" />
+                </a>
+              );
+            else if (link.name == "github")
+              return (
+                <a href={link.url} target="_blank">
+                  <FontAwesomeIcon icon={faGithub} className="icon" />
+                </a>
+              );
+            else if (link.name == "twitter")
+              return (
+                <a href={link.url} target="_blank">
+                  <FontAwesomeIcon icon={faTwitter} className="icon" />
+                </a>
+              );
+            else if (link.name == "instagram")
+              return (
+                <a href={link.url} target="_blank">
+                  <FontAwesomeIcon icon={faInstagram} className="icon" />
+                </a>
+              );
+            else if (link.name == "twitch")
+              return (
+                <a href={link.url} target="_blank">
+                  <FontAwesomeIcon icon={faTwitch} className="icon" />
+                </a>
+              );
+          })}
         </div>
       </Row>
     </Container>
